66e2473599021a8f9bda5e45a9f21eec768169d1
Lucy Lee <gamesandhannah@gmail.com>
2022-01-03 18:08:09 -0500

    Add code for pairSum(nums, target)

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,19 @@
 function pairSum(nums, target) {
-  // Insert code here;
+  if (nums.length <= 1) {
+    throw "Provided array does not contain enough distinct numbers";
+  }
+
+  let sums = [];
+
+  for (var i in nums) {
+    let j = nums.length - 1;
+    while (j != i) {
+      sums.push(nums[i] + nums[j])
+      j--
+    }
+  }
+
+  return (sums.includes(target))
 }
 
 // Do not edit this line;
