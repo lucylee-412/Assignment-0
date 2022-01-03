03292f197e02cc8d3ae22708534ada1806a0ff14
Lucy Lee <gamesandhannah@gmail.com>
2022-01-03 16:31:33 -0500

    Add code for sumOfAllOddNumbers(num)

--- 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
+++ 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
@@ -1,5 +1,13 @@
 function sumOfAllOddNumbers(nums) {
-  // Insert code here;
+  let counter = 0;
+
+  for(let i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 === 1) {
+      counter++
+    }
+  }
+
+  return counter
 }
 
 // Do not edit this line;
