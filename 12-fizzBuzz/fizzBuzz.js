378442d10cc31a5995cf2b8d69cb9069abdf9882
Lucy Lee <gamesandhannah@gmail.com>
2022-01-03 16:49:28 -0500

    Add code for fizzBuzz(start, end)

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,5 +1,22 @@
 function fizzBuzz(start, end) {
-  // Insert code here;
+  let fb = [];
+  
+  let i = start;
+  while (i <= end) {
+    if ((i % 3 == 0) && (i % 5 == 0)) {
+        fb.push("FizzBuzz");
+    } else if (i % 5 == 0) {
+      fb.push("Buzz");
+    } else if (i % 3 == 0) {
+      fb.push("Fizz");
+    } else {
+      fb.push(i);
+    }
+    
+    i++;
+  }
+  
+  return fb;
 }
 
 // Do not edit this line;
