806fb9cb42bff2fec27b2483ba9334d44fba889c
Lucy Lee <gamesandhannah@gmail.com>
2022-01-03 16:04:07 -0500

    Add code for titleCaseEdit(title)

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,24 @@
+const { compareByInspect } = require("chai/lib/chai/utils");
+
 function titleCaseEdit(title) {
-  // Insert code here;
+  title.toLowerCase()
+  // Array of words in title
+  const words = title.split(" ");
+  console.log(words)
+
+  let titleCase = "";
+
+  // Capitalize the first letter of each word
+  for (let i = 0; i < words.length; i++) {
+    titleCase += words[i].charAt(0).toUpperCase() + words[i].slice(1);
+
+    // Re-insert spaces between words
+    if (i != (words.length - 1)) {
+      titleCase += " "
+    }
+  }
+
+  return titleCase;
 }
 
 // Do not edit this line;
